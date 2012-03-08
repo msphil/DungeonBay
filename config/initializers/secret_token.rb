761d9bd88421ba684edfe8416c15a114c4f32f7d
Mike Phillips <msphil@gmail.com>
2012-03-07 20:44:04 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DungeonBay::Application.config.secret_token = '237fe4a673f4e12850fc1b761dd8b42c79bfa0dcbb1224ff22de232044c0a14e7eaf6d1b7e8341bccba013483df7acefa2cca85a90c580d2dbe8608a60777977'
