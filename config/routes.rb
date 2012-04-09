6bb42977cc15865afca840cde2a631846a84deb4
Mike Phillips <msphil@gmail.com>
2012-04-08 22:47:19 -0700

    Sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 DungeonBay::Application.routes.draw do
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
-  match '/signup', :to => 'users#new'
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   match '/contact', :to => 'pages#contact'
-  match '/about', :to => 'pages#about'
-  match '/help', :to => 'pages#help'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
 
   root :to => 'pages#home'
 
