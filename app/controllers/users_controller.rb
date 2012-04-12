bb9428dcbad356037c2f0bcc41a7a54016a39d6a
Mike Phillips <msphil@gmail.com>
2012-04-11 20:10:48 -0700

    Added campaign creation button to user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @characters = @user.characters.paginate(:page => params[:page])
+    @campaigns = @user.campaigns
     @title = @user.name
   end
 
